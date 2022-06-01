1544919538462e60f06196fffad0447d34f3efa3
Sahajpal <54036040+Sahajpal@users.noreply.github.com>
2022-06-01 16:17:18 +0530

    initialised necessary build and ui files

--- app/src/main/java/com/example/books_compose/ui/theme/Theme.kt
+++ app/src/main/java/com/example/books_compose/ui/theme/Theme.kt
@@ -5,17 +5,22 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
 
 private val DarkColorPalette = darkColors(
-        primary = Purple200,
-        primaryVariant = Purple700,
-        secondary = Teal200
+    primary = primaryNight,
+    primaryVariant = textNight,
+    secondary = Teal200,
+    onSecondary = Color.White,
+    background = backgroundNight
 )
 
 private val LightColorPalette = lightColors(
-        primary = Purple500,
-        primaryVariant = Purple700,
-        secondary = Teal200
+    primary = primary,
+    primaryVariant = text,
+    secondary = Teal200,
+    onSecondary = Color.White,
+    background = background
 
         /* Other default colors to override
     background = Color.White,
@@ -37,7 +42,7 @@ fun Books_ComposeTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Com
 
     MaterialTheme(
             colors = colors,
-            typography = Typography,
+            typography = typography,
             shapes = Shapes,
             content = content
     )
